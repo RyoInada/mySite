989eab552a5d6a662390175522608f1165954292
RyoInada <ryo.inada.sapato@gmail.com>
2022-05-26 09:43:52 +0900

    [change] the control  of getting api

--- src/customHooks/useSkills.js
+++ src/customHooks/useSkills.js
@@ -9,23 +9,88 @@ import {
 
 export const useSkills = () => {
   const [state, dispatch] = useReducer(skillReducer, initialState);
-  // const api_token = process.env.REACT_APP_GITHUB_API_KEY;
 
+  //   useEffect(() => {
+  //     dispatch({ type: actionTypes.fetch });
+  //     axios
+  //       .get("https://api.github.com/user/repos", {
+  //         headers: {
+  //           Authorization: `token ${process.env.REACT_APP_GITHUB_API_KEY}`,
+  //         },
+  //       })
+  //       .then((response) => {
+  //         console.log(response);
+  //         const languageList = response.data.map((res) => res.language);
+  //         console.log(languageList);
+  //         const countedLanguageList = generateLanguageCountObj(languageList);
+  //         dispatch({
+  //           type: actionTypes.success,
+  //           payload: { languageList: countedLanguageList },
+  //         });
+  //       })
+  //       .catch(() => {
+  //         dispatch({ type: actionTypes.error });
+  //       });
+  //   }, []);
+
+  //   const generateLanguageCountObj = (allLanguageList) => {
+  //     const notNullLanguageList = allLanguageList.filter(
+  //       (language) => language != null
+  //     );
+  //     const uniqueLanguageList = [...new Set(notNullLanguageList)];
+
+  //     return uniqueLanguageList.map((item) => {
+  //       return {
+  //         language: item,
+  //         count: allLanguageList.filter((language) => language === item).length,
+  //       };
+  //     });
+  //   };
+
+  //   const converseCountToPercentage = (count) => {
+  //     if (count > 10) {
+  //       return 100;
+  //     }
+  //     return count * 10;
+  //   };
+
+  //   const sortedLanguageList = () =>
+  //     state.languageList.sort(
+  //       (firstLang, nextLang) => nextLang.count - firstLang.count
+  //     );
+  //   return [sortedLanguageList, state.requestState, converseCountToPercentage];
+  // };
   useEffect(() => {
     dispatch({ type: actionTypes.fetch });
     axios
-      .get("https://api.github.com/user/repos", {
+      .get("https://api.github.com/users/RyoInada/repos", {
         headers: {
           Authorization: `token ${process.env.REACT_APP_GITHUB_API_KEY}`,
         },
       })
       .then((response) => {
         console.log(response);
-        const languageList = response.data.map((res) => res.language);
-        const countedLanguageList = generateLanguageCountObj(languageList);
-        dispatch({
-          type: actionTypes.success,
-          payload: { languageList: countedLanguageList },
+        const languageLink = response.data.map((res) => res.languages_url);
+        const languageList = [];
+        languageLink.forEach((url) => {
+          axios
+            .get(url, {
+              headers: {
+                Authorization: `token ${process.env.REACT_APP_GITHUB_API_KEY}`,
+              },
+            })
+            .then((res) => {
+              let keys = Object.keys(res.data);
+              keys.forEach((key) => {
+                languageList.push(key);
+                const countedLanguageList =
+                  generateLanguageCountObj(languageList);
+                dispatch({
+                  type: actionTypes.success,
+                  payload: { languageList: countedLanguageList },
+                });
+              });
+            });
         });
       })
       .catch(() => {
